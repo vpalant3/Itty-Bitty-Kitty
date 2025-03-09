6ad0e4685fab07480dcb20ddc8487a2e0e331d40
Vlad <vladislavpalant@gmail.com>
2025-03-09 12:01:12 -0500

    Initial Commit

---
+++ Itty Bitty Kitty/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Itty Bitty Kitty
+//
+//  Created by Vladislav Palant on 3/9/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
